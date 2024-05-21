e9679730226347e034cad0c5d1d7d5709a6974a2
kamalimm <kamali.d@wenxttech.com>
2024-05-21 14:25:56 +0530

    encrption and decryption

--- src/main/java/com/aic/base/commonUtils/EncryptionController.java
+++ src/main/java/com/aic/base/commonUtils/EncryptionController.java
@@ -1,28 +1,36 @@
 package com.aic.base.commonUtils;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/encryption")
 public class EncryptionController {
 
-	
 	@Autowired
-    private EncryptionService encryptionService;
+	private EncryptionService encryptionService;
 
-    @PostMapping("/encrypt")
-    public String encrypt(@RequestBody EncryptionRequest request) {
-        return encryptionService.encryptstr(request.getValue());
-    }
+	@PostMapping(value = "/encrypt", consumes = "application/json", produces = "application/json")
+	public String encrypt(@RequestBody String request) {
 
-    @PostMapping("/decrypt")
-    public String decrypt(@RequestBody EncryptionRequest request) {
-        return encryptionService.decryptstr(request.getValue());
-    }
+		JSONObject input = new JSONObject(request);
+		String encryptedValue = encryptionService.encryptstr(input.getString("userPass"));
+		JSONObject response = new JSONObject();
+		response.put("Data", encryptedValue);
+		return response.toString();
+	}
 
+	@PostMapping(value = "/decrypt", consumes = "application/json", produces = "application/json")
+	public String decrypt(@RequestBody String request) {
+		JSONObject input = new JSONObject(request);
+		String encryptedValue = encryptionService.decryptstr(input.getString("Youpass"));
+		JSONObject response = new JSONObject();
+		response.put("Data", encryptedValue);
+		return response.toString();
+
+	}
 }
