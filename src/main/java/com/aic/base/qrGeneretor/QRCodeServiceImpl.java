c8af205c3d24f6e5a706321a8e60f68e9a9e0051
kamalimm <kamali.d@wenxttech.com>
2024-06-21 16:59:18 +0530

    CHANGES FOR QR GENERATOR SEQUENECE

--- src/main/java/com/aic/base/qrGeneretor/QRCodeServiceImpl.java
+++ src/main/java/com/aic/base/qrGeneretor/QRCodeServiceImpl.java
@@ -1,111 +1,103 @@
 package com.aic.base.qrGeneretor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import com.spire.barcode.BarCodeGenerator;
+import com.spire.barcode.BarCodeType;
+import com.spire.barcode.BarcodeSettings;
+import com.spire.barcode.QRCodeECL;
+
+import jakarta.annotation.PostConstruct;
+
+import org.json.JSONObject;
+
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
-import java.util.Random;
-import javax.imageio.ImageIO;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import com.spire.barcode.BarCodeGenerator;
-import com.spire.barcode.BarCodeType;
-import com.spire.barcode.BarcodeSettings;
-import com.spire.barcode.QRCodeECL;
-
 
 @Service
 public class QRCodeServiceImpl implements QRCodeService {
 
-	@Value("${spring.message.code}")
-	private String messageCode;
-
-	@Value("${spring.status.code}")
-	private String statusCode;
-
-	@Value("${spring.data.code}")
-	private String dataCode;
-
-	@Value("${spring.data.filepath}")
-	private String locationCode;
-	
-	@Value("${spring.success.code}")
-	private String successCode;
-
-	@Value("${spring.error.code}")
-	private String errorCode;
-
-	@Value("${spring.warning.code}")
-	private String warningCode;
-	
 	private static final String BASE_PATH = "D:/QRGenerator/";
+	private static final String SEQUENCE_NAME = "QR_CODE_SEQ";
+	private JdbcTemplate jdbcTemplate;
 
-	@Override
-    public ResponseEntity<String> generateQRCode(QRCodeRequestDto qrCodeRequest) throws IOException {
-        BarcodeSettings settings = new BarcodeSettings();
-        settings.setType(BarCodeType.QR_Code);
+	@Autowired
+	public QRCodeServiceImpl(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
-        StringBuilder dataString = new StringBuilder();
-        for (Map.Entry<String, String> entry : qrCodeRequest.getData().entrySet()) {
-            dataString.append(entry.getKey()).append(": ").append(entry.getValue()).append(", ");
-        }
+	@PostConstruct
+	public void initialize() {
+		createSequenceIfNotExists();
+	}
 
-        if (dataString.length() > 0) {
-            dataString.setLength(dataString.length() - 2);
-        }
+	private void createSequenceIfNotExists() {
+		String checkSequenceQuery = "SELECT COUNT(*) FROM user_sequences WHERE sequence_name = ?";
+		Integer count = jdbcTemplate.queryForObject(checkSequenceQuery, new Object[] { SEQUENCE_NAME.toUpperCase() },
+				Integer.class);
 
-        settings.setData(dataString.toString());
-        settings.setX(2);
-        settings.setQRCodeECL(QRCodeECL.M);
-        settings.setTopText(qrCodeRequest.getTopText());
-        settings.setBottomText(qrCodeRequest.getBottomText());
-        settings.setShowText(false);
-        settings.setShowTopText(true);
-        settings.setShowBottomText(true);
-        settings.hasBorder(false);
+		if (count == null || count == 0) {
+			String createSequenceQuery = "CREATE SEQUENCE " + SEQUENCE_NAME + " START WITH 1 INCREMENT BY 01";
+			jdbcTemplate.execute(createSequenceQuery);
+		}
+	}
 
-        BarCodeGenerator barCodeGenerator = new BarCodeGenerator(settings);
-        BufferedImage bufferedImage = barCodeGenerator.generateImage();
+	public ResponseEntity<String> generateQRCode(QRCodeRequestDto qrCodeRequest) throws IOException {
+		BarcodeSettings settings = new BarcodeSettings();
+		settings.setType(BarCodeType.QR_Code);
 
-        
-        String uniqueId = generateFourDigitId();
-        
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); 
-        String timestamp = dateFormat.format(new Date()); 
-        String fileName = uniqueId + "Wenxt"+ timestamp + ".png"; 
+		StringBuilder dataString = new StringBuilder();
+		for (Map.Entry<String, String> entry : qrCodeRequest.getData().entrySet()) {
+			dataString.append(entry.getKey()).append(": ").append(entry.getValue()).append(", ");
+		}
 
-        
-        String filePath = BASE_PATH + fileName;
-        File outputFile = new File(filePath);
-        ImageIO.write(bufferedImage, "png", outputFile);
+		if (dataString.length() > 0) {
+			dataString.setLength(dataString.length() - 2);
+		}
 
-       
-        JSONObject response = new JSONObject();
-        
-        JSONObject data = new JSONObject();
-        response.put(statusCode, successCode);
-        response.put(messageCode, "QR code generated successfully");
-        data.put(locationCode, filePath);
-        response.put(dataCode, data);
+		settings.setData(dataString.toString());
+		settings.setX(2);
+		settings.setQRCodeECL(QRCodeECL.M);
+		settings.setTopText(qrCodeRequest.getTopText());
+		settings.setBottomText(qrCodeRequest.getBottomText());
+		settings.setShowText(false);
+		settings.setShowTopText(true);
+		settings.setShowBottomText(true);
+		settings.hasBorder(false);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+		BarCodeGenerator barCodeGenerator = new BarCodeGenerator(settings);
+		BufferedImage bufferedImage = barCodeGenerator.generateImage();
 
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(response.toString());
-    }
+		// Generate unique ID using the sequence
+		long uniqueId = jdbcTemplate.queryForObject("SELECT " + SEQUENCE_NAME + ".NEXTVAL FROM DUAL", Long.class);
 
-    private String generateFourDigitId() {
-        Random random = new Random();
-        int id = random.nextInt(10000); 
-        return String.format("%02d", id); 
-    }
+	
+		String fileName = "WENXT" +"0"+ uniqueId+ ".png"; // Construct file name
+
+		// Define the path where the image will be saved
+		String filePath = BASE_PATH + fileName;
+		File outputFile = new File(filePath);
+		ImageIO.write(bufferedImage, "png", outputFile);
+
+		// Create JSON response
+		JSONObject response = new JSONObject();
+		response.put("statusCode", "200");
+		response.put("message", "QR code generated successfully");
+		response.put("filePath", filePath);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		return ResponseEntity.ok().headers(headers).body(response.toString());
+	}
 }
-
