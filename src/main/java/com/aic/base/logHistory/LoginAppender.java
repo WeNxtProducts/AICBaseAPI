cbb755190685150beab43c966db9673eb6942c67
ragul-wenxt <ragul.arulkumaran@wenxttech.com>
2025-08-06 15:21:08 +0530

    as

--- src/main/java/com/aic/base/logHistory/LoginAppender.java
+++
@@ -1,9 +0,0 @@
-package com.aic.base.logHistory;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-public interface LoginAppender {
-
-	public void loginToLJMLogs1(String message, HttpServletRequest request, String name);
-
-}
--- src/main/java/com/aic/base/logging/LoggerFunctionService.java
+++
@@ -1,9 +0,0 @@
-package com.aic.base.logging;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-public interface LoggerFunctionService {
-
-	void logToEmailHistoryLogs(EmailLogsDTO logs, HttpServletRequest request);
-
-}
